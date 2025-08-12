a2454d6f75b841a37cd6fd197e79873f019bfe6d
abergman <abergman@student.42.fr>
2025-08-12 14:19:37 +0200

    feat(*): added inports

---
+++ mini_serv_1.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
